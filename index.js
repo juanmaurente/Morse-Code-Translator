34af576d7b2a5f6e72c49bec7efd6323aababfd9
Juan Maurente <juanmaurente@gmail.com>
2022-07-23 21:44:34 +1000

    Styling and eng to morse working

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
-export const sum = (a, b) => {
-	return a + b;
+const enInput = document.querySelector('#en_input');
+const enButton = document.querySelector('#en_button');
+const enResult = document.querySelector('#en_result');
+const morseInput = document.querySelector('#mor_input');
+const morseButton = document.querySelector('#mor_button');
+const morseResult = document.querySelector('#mor_result');
+
+const alphabetToMorse = {
+	A: '.-',
+	B: '-...',
+	C: '-.-.',
+	D: '-..',
+	E: '.',
+	F: '..-.',
+	G: '--.',
+	H: '....',
+	I: '..',
+	J: '.---',
+	K: '-.-',
+	L: '.-..',
+	M: '--',
+	N: '-.',
+	O: '---',
+	P: '.--.',
+	Q: '--.-',
+	R: '.-.',
+	S: '...',
+	T: '-',
+	U: '..-',
+	V: '...-',
+	W: '.--',
+	X: '-..-',
+	Y: '-.--',
+	Z: '--..',
+	' ': ' ',
+};
+
+enButton.addEventListener('click', () => {
+	let engText = enInput.value;
+	engText.toUpperCase;
+	translateToMorse(engText);
+});
+
+const translateToMorse = (str) => {
+	const alphaArray = Object.entries(alphabetToMorse);
+	const result = [];
+	console.log(alphaArray);
+	[...str].map((char) => {
+		result.push(Object.values(alphabetToMorse[char]).join(''));
+		return;
+	});
+	enResult.innerText = result.join('   ');
 };
